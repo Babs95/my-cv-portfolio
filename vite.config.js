c5038819f9deecfb01b3d61890d8ce3fc09642cf
Babacar NDIAYE <bndiaye@modelsis.sn>
2025-11-26 19:38:29 +0000

    deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-cv-portfolio/',
   optimizeDeps: {
     include: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts']
   }
